23fb10abef31e2e8a41707fb09a8a28c6217c4c5
Hank Emmanuel Nixon <hankenixon456@gmail.com>
2025-01-29 21:36:18 +0400

    Fixed API Changes, API Working

--- Backend/prompts/prompt.py
+++ Backend/prompts/prompt.py
@@ -1,18 +1,22 @@
 class Prompt:
     def __init__(self):
         self.syllabus_prompt = '''<user>
-            Extract the syllabus from the given PDF in the following formatted JSON Structure
+            Extract the syllabus from the given the syllabus text:
+            
+            {syllabus_text}
+            
+            Return it in the following formatted JSON Structure:
             <JSON>
-                {
-                Subject : *hint: Extract Subject name from the pdf*
-                Syllabus: [
-                {
-                module: *hint: Extract module heading*,
-                subject: *hint: Make a headline for the module based on the topics and subtopics*
-                topic: *hint: Extract the topic names from the syllabus and store it as a list*
-                }
+                {{
+                "Subject": "Extract Subject name from the pdf"
+                "Syllabus": [
+                {{
+                "module": "Extract module heading",
+                "subject": "Make a headline for the module based on the topics and subtopics"
+                "topic": ["Extract the topic names from the syllabus and store it as a list"]
+                }}
                 ]
-             }
+             }}
         </JSON>
         </user>'''
 
